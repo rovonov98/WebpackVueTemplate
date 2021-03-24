4976a70796ec6f74cb141a2122a99ffba0025fd1
rovonov98 <stariyakk121@gmail.com>
2021-03-24 21:18:28 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+      <div class="flex">{{ someData }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            someData: 'it works'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.flex {
+    font-size: $font-size;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const { CleanWebpackPlugin }  = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = (_, { mode }) => ({
+    entry: path.join(__dirname, 'src', 'main.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    mode === 'production'
+                    ? MiniCssExtractPlugin.loader
+                    : 'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            additionalData: '@import "src/assets/scss/variables.scss";',
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ]
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin(),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+})
